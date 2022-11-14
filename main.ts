c4e29cc52190ff33fd41838dbe1e3f58d3e49872
THOMASLeumg <115450612+THOMASLeumg@users.noreply.github.com>
2022-11-14 14:54:03 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.compassHeading() == 0) {
+        basic.showString("N")
+    } else if (input.compassHeading() == 90) {
+        basic.showString("E")
+    } else if (input.compassHeading() == 180) {
+        basic.showString("S")
+    } else if (input.compassHeading() == 270) {
+        basic.showString("W")
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() < 359 && (input.compassHeading() >= 315 || input.compassHeading() <= 45)) {
+        basic.showString("N")
+    } else if (input.compassHeading() < 90 && (input.compassHeading() > 45 || input.compassHeading() < 135)) {
+        basic.showString("E")
+    } else if (input.compassHeading() < 180 && (input.compassHeading() > 135 || input.compassHeading() < 225)) {
+        basic.showString("S")
+    } else if (input.compassHeading() != 270 && (input.compassHeading() > 225 || input.compassHeading() < 315)) {
+        basic.showString("W")
+    }
+})
